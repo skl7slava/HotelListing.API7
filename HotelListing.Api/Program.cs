22bb7d6a425d9f3aeca19f93246742278ce42ce4
skl7slava <livrare1@gmail.com>
2024-03-15 15:55:14 +0200

    .

--- HotelListing.Api/Program.cs
+++ HotelListing.Api/Program.cs
@@ -113,11 +113,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   
 }
 app.UseSwagger();
-app.UseSwaggerUI()
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionMiddleware>();   
 app.UseHttpsRedirection();
